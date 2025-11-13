a79254800353adb4402e3d7747872db38689d286
Dave Allison <david.s.allison@gmail.com>
2025-11-13 15:22:02 -0800

    Remove generated crap

--- plugin/main.cc
+++
@@ -1,7 +0,0 @@
-#include <google/protobuf/compiler/plugin.h>
-#include "plugin/ros_generator.h"
-
-int main(int argc, char* argv[]) {
-  sato::RosGenerator generator;
-  return google::protobuf::compiler::PluginMain(argc, argv, &generator);
-}
