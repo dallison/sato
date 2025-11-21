8c22b8c37b86a3feefe58c514e36a4b854622e3b
Dave Allison <dave.allison@getcruise.com>
2025-11-20 17:51:56 -0800

    Try to include neutron generated header

--- sato/sato_basic_test.cc
+++ sato/sato_basic_test.cc
@@ -3,8 +3,16 @@
 // Copyright (C) 2025 David Allison.  All Rights Reserved.
 
 #include "absl/strings/str_format.h"
+
+// Protobuf messages.
 #include "sato/testdata/TestMessage.pb.h"
+
+// Sato conversion classes.
 #include "sato/testdata/TestMessage.sato.h"
+
+// Neutron generated messages
+#include "sato/serdes/test_msgs/TestMessage.h"
+
 #include "toolbelt/hexdump.h"
 #include <gtest/gtest.h>
 #include <sstream>
@@ -192,3 +200,50 @@ TEST(SatoBasicTest, Any) {
   ASSERT_EQ(serialized_size, proto_serialized_size);
 }
 
+TEST(SatoBasicTest, ProtoToROS) {
+  foo::bar::TestMessage msg;
+  msg.set_x(1234);
+  msg.set_s("hello world");
+  msg.add_vi32(1);
+  msg.add_vi32(2);
+  msg.add_vi32(3);
+
+  msg.add_vstr("one");
+  msg.add_vstr("two");
+  msg.add_vstr("three");
+
+  auto m = msg.mutable_m();
+  m->set_str("Inner message");
+  m->set_f(1234567890);
+
+  auto vm = msg.mutable_vm();
+  auto inner = vm->Add();
+  inner->set_str("Inner1");
+  inner->set_f(999);
+
+  inner = vm->Add();
+  inner->set_str("Inner2");
+  inner->set_f(888);
+
+  msg.set_e(foo::bar::FOO);
+
+  msg.set_u1a(0x01020304);
+
+  size_t proto_serialized_size = msg.ByteSizeLong();
+
+  std::string serialized;
+  msg.SerializeToString(&serialized);
+  ASSERT_EQ(1234, msg.x());
+  ASSERT_EQ(0, msg.y());
+  ASSERT_EQ("hello world", msg.s());
+  toolbelt::Hexdump(serialized.data(), serialized.size(), stderr);
+
+  foo::bar::sato::TestMessage t;
+  sato::ProtoBuffer buffer(serialized);
+  sato::ROSBuffer ros_buffer;
+  absl::Status status = t.ProtoToROS(buffer, ros_buffer, 0x1234567890abcdef);
+  std::cerr << "status: " << status << std::endl;
+  ASSERT_TRUE(status.ok());
+
+  
+}
